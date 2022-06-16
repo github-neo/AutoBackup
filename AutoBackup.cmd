b8661204caccd22d4ce8ff6c4b8d1b4d3ea5fe1c
yaolin <yaolinforward@qq.com>
2022-06-17 00:22:40 +0800

    init commit

---
+++ AutoBackup.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+rem Set UTF-8
+chcp 65001
+
+SET BACKUP_PATH=%~dp0backups\
+SET BAT_PATH="%~dp0utils\doBackup.cmd"
+SET EXE_CMD_PATH="C:\Program Files\7-Zip\7z"
+
+echo BACKUP_PATH=%BACKUP_PATH%
+echo BAT_PATH=%BAT_PATH%
+echo EXE_CMD_PATH=%EXE_CMD_PATH%
+echo.
+
+echo ====== Start AutoBackup. ======
+echo.
+
+for /f "eol=# tokens=1,2 delims==" %%i in (BackupList.properties) do echo.call %BAT_PATH% "%%i" "%%j" && call %BAT_PATH% "%%i" "%%j" && echo.
+
+echo ====== Finish AutoBackup. ======
---
+++ utils/doBackup.cmd
@@ -0,0 +1,29 @@
+@echo off
+
+rem archive name without extension name.
+SET ARCHIVE_NAME=%~1
+
+rem source path of backup
+SET SOURCE_PATH=%~2
+
+echo.
+echo ****** Start backup %ARCHIVE_NAME% ******
+echo ARCHIVE_NAME=%ARCHIVE_NAME%
+echo SOURCE_PATH=%SOURCE_PATH%
+echo.
+
+if exist "%BACKUP_PATH%%ARCHIVE_NAME%.7z" (
+	echo del "%BACKUP_PATH%%ARCHIVE_NAME%_0.7z"
+	del "%BACKUP_PATH%%ARCHIVE_NAME%_0.7z"
+	echo ren "%BACKUP_PATH%%ARCHIVE_NAME%.7z" "%ARCHIVE_NAME%_0.7z"
+	ren "%BACKUP_PATH%%ARCHIVE_NAME%.7z" "%ARCHIVE_NAME%_0.7z"
+	echo.
+)
+
+echo %EXE_CMD_PATH% a "%BACKUP_PATH%%ARCHIVE_NAME%.7z" "%SOURCE_PATH%" -mx9 -ms=off -mmt
+%EXE_CMD_PATH% a "%BACKUP_PATH%%ARCHIVE_NAME%.7z" "%SOURCE_PATH%" -mx9 -ms=off -mmt
+echo.
+
+echo.
+echo ****** Finish backup %ARCHIVE_NAME% ******
+echo.
